55453eb8293259ca5e3691d02d31bbe36c4854a5
Samuel <oiranca@gmail.com>
2024-09-30 12:59:10 +0100

    wip:social networks component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { Navbar } from './components/Navbar/Navbar.tsx';
+import { SocialNetworks } from './components/SocialNetworks/SocialNetworks.tsx';
 
 function App() {
     return (
         <>
             <Navbar />
+            <SocialNetworks />
         </>
     );
 }
---
+++ src/components/SocialNetworks/SocialNetworks.tsx
@@ -0,0 +1,11 @@
+import { XIcon } from './components/XIcon/XIcon.tsx';
+import { LinkedinIcon } from './components/LinkedinIcon/LinkedinIcon.tsx';
+
+export const SocialNetworks = () => {
+    return (
+        <section>
+            <XIcon />
+            <LinkedinIcon />
+        </section>
+    );
+};
