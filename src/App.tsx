0072279f87c3a04efe2c197b78e910f5cae0b03c
Samuel <oiranca@gmail.com>
2024-10-02 10:27:30 +0100

    created SocialNetworks component and integration in navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css';
 import { Navbar } from './components/Navbar/Navbar.tsx';
-import { SocialNetworks } from './components/SocialNetworks/SocialNetworks.tsx';
 
 function App() {
     return (
         <>
             <Navbar />
-            <SocialNetworks />
         </>
     );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,29 @@
 import '../../styles/Navbar/Navbar.css';
 import { MobileMenu } from './components/MobileMenu/MobileMenu.tsx';
+import { SocialNetworks } from '../SocialNetworks/SocialNetworks.tsx';
 
 export const Navbar = () => {
     return (
         <nav className="navbar_container">
-            <ul className="list_menu">
-                <li className="item_menu">
-                    <a href="#about">Sobre mí</a>
-                </li>
-                <li className="item_menu">
-                    <a href="#experience">Experiencia</a>
-                </li>
-                <li className="item_menu">
-                    <a href="#projects">Proyectos</a>
-                </li>
-                <li className="item_menu">
-                    <a href="#contact">Contacto</a>
-                </li>
-            </ul>
+            <section className="section_menu_desktop">
+                <ul className="list_menu">
+                    <li className="item_menu">
+                        <a href="#about">Sobre mí</a>
+                    </li>
+                    <li className="item_menu">
+                        <a href="#experience">Experiencia</a>
+                    </li>
+                    <li className="item_menu">
+                        <a href="#projects">Proyectos</a>
+                    </li>
+                    <li className="item_menu">
+                        <a href="#contact">Contacto</a>
+                    </li>
+                </ul>
+            </section>
+            <section className="section_menu_desktop">
+                <SocialNetworks />
+            </section>
             <MobileMenu />
         </nav>
     );
