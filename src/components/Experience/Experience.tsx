be8da229d4e54e7da2fa322341f286a8d42a003f
Samuel <oiranca@gmail.com>
2024-10-07 13:11:00 +0100

    wip: style experience component

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -4,7 +4,7 @@ export const Experience = () => {
     const icon = 'experience/experience.png';
     return (
         <div className="Experience">
-            <header className='experience-header'>
+            <header className="experience-header">
                 <img
                     className="experience-icon"
                     src={icon}
@@ -14,11 +14,33 @@ export const Experience = () => {
 
             </header>
             <article className="experience-body">
-                <section className='experience-item'>
-                    <img src='public/experience/auj-logo.png' alt='Logo de la Aosiciaón Adoptaun Un Junior'/>
-                    <h3 className='title-item'>
-                        #AdoptaUnJunior
-                    </h3>
+                <section className="experience-item">
+                    <header>
+                        <img src="public/experience/auj-logo.png" alt="Logo de la Asociación Adoptaun Un Junior" />
+                        <h3 className="title-item">
+                            #AdoptaUnJunior
+                        </h3>
+                    </header>
+                    <section>
+                        <p className="description-item">
+                            Desarrollador Frontend voluntario en la Asociación AdoptaUnJunior </p>
+                    </section>
+                    <footer>
+                        <ul className="list-technologies">
+                            <li className='technologies-item'>
+                                React
+                            </li>
+                            <li className='technologies-item'>
+                                Next.js
+                            </li>
+                            <li className='technologies-item'>
+                                TypeScript
+                            </li>
+                            <li className='technologies-item'>
+                                TailwindCSS
+                            </li>
+                        </ul>
+                    </footer>
                 </section>
             </article>
         </div>
