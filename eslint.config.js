e68da0a71a879f04c7076d24518812b957954eb5
oiranca <samuelromeroarbelo@gmail.com>
2024-11-25 10:00:43 +0000

    fixed eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -9,6 +9,7 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 export default tseslint.config(
     { ignores: ['dist'] },
     {
+        settings: { react: { version: '18.0' } },
         extends: [
             js.configs.recommended,
             ...tseslint.configs.recommendedTypeChecked,
